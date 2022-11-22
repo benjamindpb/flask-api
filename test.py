4b93ed23b58e37076deace429e3c74b9dea94831
benjamindpb <benjamin.dpb@gmail.com>
2022-11-22 13:53:43 -0400

    algorithm test for labels search implemented

--- test.py
+++ test.py
@@ -1,5 +1,7 @@
-from datetime import date
 from utils import *
+from app import *
+import time
+import pandas as pd
 
 def performance_test():
     N = [10**2, 10**3, 10**4, 10**5, 10**6, 10**7]
@@ -19,10 +21,38 @@ def content_test():
     for d in dates:
         get_new_dump(filename=d+'-latest-truthy.nt', complete_dump=True)
         entity_dict, _ = entities_with_coords()
-        types_dict, types_list, _ = instances_of_entities(entity_dict)
-        get_tsv(types_dict, types_list, f'test/content/{d}-types.tsv')
+        types_dict, types_list, _ = instances_of_entities(entity_dict)  # type: ignore
+        get_tsv(types_dict, types_list, f'test/content/{d}-types.tsv')  # type: ignore
         time.sleep(600)
         
+def search_label_test():
+    print("Executing search_label_test()...")
+    _start = time.time()
+    with open('p31/types.json', 'r') as f, open('test/labels/labels_perf.tsv', 'w', encoding="utf-8") as lp:
+        tjson = json.loads(f.read(), strict=False)
+        lp.write('id\tlabel\ttime\tresults\n')
+        # n = 0
+        for item in tjson["types"].items():
+            # if n == 20: break
+            label = item[1]['label']
+            start = time.time()
+            results = data(label)
+            end = time.time()
+            search_time = round(end-start, 3)
+            count = results["count"] if 'count' in results else 0
+            lp.write(f'{item[0]}\t{label}\t{search_time}\t{count}\n')
+            # n += 1
+    _end = time.time()
+    total_time = round(_end-_start, 3)
+    print(f"\nTotal exec time: {total_time}")
+
+
+
 
 if __name__ == '__main__':
-    content_test()
+    # search_label_test()
+    df = pd.read_csv("test/labels/labels_perf.tsv", sep='\t')
+    errors = df[df['results'] < 0]
+    with open('test/labels/errors.tsv', 'w') as f:
+        for 
+    print(errors.head())
